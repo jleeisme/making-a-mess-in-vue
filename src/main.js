89cdd3e714bac12fd09b71448244e89da88366f7
Jason Lee <jleeisme@gmail.com>
2017-01-23 23:20:53 -0800

    added an input component
    added through the crap component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import Crap from './Crap.vue'
 
+Vue.component('app-crap', Crap)//this is now a global component
+
 new Vue({
-  el: '#something',
-  render: h => h(App)
+  el: '#app',//the div in index.html which we change
+  render: h => h(App)//replace with some other code
+  // overrides to not use the DOM as template but as a hook
+  // other templates can be used
 })
