51be6be21929d765fb6c9530eaeac8c6d862a0cb
Jason Lee <jleeisme@gmail.com>
2017-02-07 17:13:04 -0800

    added another component
    haven't figured on using separate transitions for different routes yet

---
+++ src/components/End.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h3>End of the line pal</h3>
+  </div>
+</template>
--- src/components/Losers.vue
+++ src/components/Losers.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
-    <h2>I'm a loser</h2>
+      <h2>I'm a loser</h2>
+            
     <!-- <button @click="goLosers">Go to the losers</button> -->
-    <router-link to="/users">To users components</router-link>
+    <!-- <transition name="slide-fade"> -->
+      <router-link to="/end">Go to a dead end</router-link>
+    <!-- </transition> -->
+        
   </div>
 </template>
+
+<style >
+  /*.slide-fade-enter-active {
+  transition: all .3s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to {
+    transform: translateX(10px);
+    opacity: 0;
+  }  */
+</style>
 
