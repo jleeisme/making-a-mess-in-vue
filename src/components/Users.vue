276176757d7adf6ec29400f105e628da4d053149
Jason Lee <jleeisme@gmail.com>
2017-02-07 17:18:05 -0800

    changed the users link to a button

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -2,13 +2,12 @@
   <div>
     <h2>This is Users</h2>
     <button @click="goHome">To Home component</button>
-    <button @click="goLosers">Go to Losers</button>
-    <router-link to="/hello">To Hello component</router-link>
+    <button @click="goLosers">Go to Losers component</button>
+    <button @click="goHello">Go to Hello component</button>
   </div>
 </template>
 
 <script>
-// import Losers from './Losers.vue'
 
 export default {
   methods: {
@@ -17,6 +16,9 @@ export default {
     },  
     goLosers() {
       this.$router.push('/losers')
+    },
+    goHello() {
+      this.$router.push('/hello')
     }
   }
 }
