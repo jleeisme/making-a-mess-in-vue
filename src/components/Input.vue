a5a84d00a14ce7f77f7bee3126cf467026a412db
Jason Lee <jleeisme@gmail.com>
2017-02-10 20:08:30 -0800

    addde a computed property
    just practice for using computed and listening for events

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -4,9 +4,10 @@
       <p>Input component called through the Hook component</p>
       <p class="text">{{ text }}</p>
       <input type="text" v-model="message" id="user-input" placeholder="No typing" v-on:keyup="pressed">
-      <router-link to="/users">To Users component</router-link>
       <!-- to= can be bound by :to and then direct to the link via a variable, but this is the whole path, so for now nbd -->
       <p class="message">{{ message }}</p>
+      <p class="order">{{ order }}</p>
+      <router-link to="/users">To Users component</router-link>
     </form>
   </div>
 </template>  
@@ -22,6 +23,11 @@
     methods: { 
       pressed(e) {
         console.log(e.target.value)
+      }
+    },
+    computed: {
+      order() {
+        return this.message != `stop` ? `What's the opposite of go?` : `Stop!`
       }
     }
   }
