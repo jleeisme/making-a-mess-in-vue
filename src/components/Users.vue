c00745e2cdf401267788fbcbe000706a1a31d9d9
Jason Lee <jleeisme@gmail.com>
2017-02-14 16:55:55 -0800

    Added computed to switch classes
    Toggles between classes on clicks

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -5,15 +5,12 @@
     <button @click="goHome">Go to Home component</button>
     <button @click="goLosers">Go to Losers component</button>
     <button @click="goHello">Go to Hello component</button>
-    <div class="box" @click="attachColour = !attachColour" :class="{black: attachColour}"> Click </div>
+    <div class="box" @click="attachColour = !attachColour" :class="switchClasses"> Click </div>
 
-  </div>
-    
-  
+  </div>  
 </template>
 
 <script>
-
 export default {
   methods: {
     goHome() {
@@ -28,23 +25,38 @@ export default {
   },
   data() {
     return {
+      // color: 'red',
       attachColour: false
+    }
+  },
+  computed: {
+    switchClasses() {
+      return {
+        red: this.attachColour,
+        black: !this.attachColour
+      }
     }
   }
 }
-
 </script>
 
 <style>
   .box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     height: 100px;
     width: 100px;
-    background-color: grey;
+/*    background-color: grey;*/
     margin: 10px auto;
     color: white;
   }
 
   .black {
     background-color: black;
+  }
+
+  .red {
+    background-color: red;
   }
 </style>
