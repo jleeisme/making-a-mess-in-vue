ec34b5666afe4a718b139c3922c670fe11b42761
Jason Lee <jleeisme@gmail.com>
2017-02-19 22:45:11 -0800

    changed some formatting
    pushing before i forget

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -5,10 +5,14 @@
     <button @click="goHome">Go to Home component</button>
     <button @click="goLosers">Go to Losers component</button>
     <button @click="goHello">Go to Hello component</button>    
-    <div class="box" @click="attachColour = !attachColour" :class="switchClasses" :style="[boxChange, {height: width + 'px'}]"> Click... !> 300 </div>
-    <!-- in the array, the height is set to be what the width has been set to. -->
+    
+    <div class="box" @click="attachColour = !attachColour" 
+      :class="switchClasses" :style="[boxChange, {height: width + 'px'}]">
+      <!-- in the array, the height is set to be what the width has been set to. --> 
+      Click... !> 300 
+    </div>
+    
     <input type="text" v-model="width" placeholder="adjust the box width">
-
   </div>  
 </template>
 
@@ -52,6 +56,8 @@ export default {
   .box {
     max-width: 300px;
     max-height: 300px;
+    min-width: 50px;
+    min-height: 50px;
     display: flex;
     align-items: center;
     justify-content: center;
